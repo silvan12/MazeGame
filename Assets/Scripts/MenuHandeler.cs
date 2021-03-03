258756a383b18904e0125ea77b2444188997652a
silvan12 <ikbijtkijkuit05@gmail.com>
2021-03-03 20:09:54 +0100

    Change movement speed

---
+++ Assets/Scripts/MenuHandeler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandeler : MonoBehaviour
+{
+    public void QuitGame() {
+        Application.Quit ();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
