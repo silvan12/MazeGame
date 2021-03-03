d7ff327f9eb4f784396da0bf4af804979e94bed5
silvan12 <ikbijtkijkuit05@gmail.com>
2021-03-03 21:10:41 +0100

    Change movement; now turns player and forward is relative to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public float moveSpeed;
+    public float rotationSpeed;
     void FixedUpdate()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        
+        // Adds relative Y force to player
         float moveY = Input.GetAxis("Vertical");
+        Vector2 playerPosition = new Vector2(0, moveY);
+        rb2D.AddRelativeForce(playerPosition * moveSpeed);
+        
+        // Rotation
+        float rotateZ = Input.GetAxis("Horizontal");
+        transform.Rotate(0,0, rotateZ * rotationSpeed, Space.Self);
 
-        Vector2 playerPosition = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
-        rb2D.AddForce(playerPosition);
     }
 }
