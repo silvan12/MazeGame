56a605566f88ab1a3248a1cbc0164d891a1e6c6d
silvan12 <ikbijtkijkuit05@gmail.com>
2021-03-03 17:47:44 +0100

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float moveSpeed;
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 playerPosition = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+        rb2D.AddForce(playerPosition);
+    }
+}
